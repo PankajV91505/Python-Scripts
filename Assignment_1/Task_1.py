72068d9e222ada493593168f35c200d6f804c519
Pankaj Verma <pankajv91505@gmail.com>
2025-03-31 00:24:17 +0530

    Add basic arithmetic operations and greeting script

---
+++ Assignment_1/Task_1.py
@@ -0,0 +1,22 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 == 0:
+        return "Cannot divide by zero"
+    
+    return num1 / num2
+
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", multiply(num1, num2))
+print("Division:", divide(num1, num2))
---
+++ Assignment_1/Task_2.py
@@ -0,0 +1,7 @@
+name = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+
+res = f"hello, {name} {lname}! Welcome to the world of Python!"
+print(res)
+
+

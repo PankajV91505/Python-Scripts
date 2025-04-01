83edb3b7cf7a1f5062793db0e20ece41044438cd
Pankaj Verma <pankajv91505@gmail.com>
2025-04-01 11:21:59 +0530

    Remove Task_2.py as it contains unused code for list extraction and manipulation

--- Assignment_5/Task_2.py
+++
@@ -1,8 +0,0 @@
-numbers = list(range(1, 11))
-
-extracted_list = numbers[0:5]
-
-reversed_list = extracted_list[::-1]
-
-print("Extracted first five element:", extracted_list)
-print("Reversed extracted element:", reversed_list)

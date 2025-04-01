6105d41b2ad70fdb724690e28886780c205e7177
Pankaj Verma <pankajv91505@gmail.com>
2025-04-01 11:24:19 +0530

    Add task_2.py with list extraction and manipulation examples

---
+++ Assignment_5/task_2.py
@@ -0,0 +1,8 @@
+numbers = list(range(1, 11))
+
+extracted_list = numbers[0:5]
+
+reversed_list = extracted_list[::-1]
+
+print("Extracted List:", extracted_list)
+print("Reversed List:", reversed_list)

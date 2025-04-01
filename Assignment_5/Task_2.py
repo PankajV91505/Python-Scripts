fc1e9ebdde9253561e9c2f2c1ac91fa7c6b1bdc3
Pankaj Verma <pankajv91505@gmail.com>
2025-04-01 11:18:45 +0530

    Remove print statements for extracted and reversed lists in Task_2.py

--- Assignment_5/Task_2.py
+++ Assignment_5/Task_2.py
@@ -4,6 +4,5 @@ extracted_list = numbers[0:5]
 
 reversed_list = extracted_list[::-1]
 
-# Step 4: Print both lists
 print("Extracted first five element:", extracted_list)
 print("Reversed extracted element:", reversed_list)
